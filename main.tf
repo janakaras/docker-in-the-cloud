f19877f987490d99501eb4c4e804365d2f314f02
janakaras <94690242+janakaras@users.noreply.github.com>
2022-04-05 10:53:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,20 @@
-resource "null_resource" "example" {
- triggers = {
-  value = "A example resource that does nothing!"
- }
-}
+terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "test-organization-cloud-computing"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "docker-in-the-cloud"
+         }
+       }
+     }
+
+     # An example resource that does nothing.
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }
+
 
