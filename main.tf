8a56a0dcc35428c409bfea8d39a299e4da20d1f7
janakaras <94690242+janakaras@users.noreply.github.com>
2022-04-05 10:45:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-# An example resource that does nothing.
-
-terraform {
- required_providers {
-  null = {
-   version = "~> 3.0.0"
-  }
- }
-}
-
-
 resource "null_resource" "example" {
  triggers = {
   value = "A example resource that does nothing!"
