0a5ae5281fa4ba062bd15bd1d27bdc0546327349
janakaras <94690242+janakaras@users.noreply.github.com>
2022-04-05 10:38:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 # An example resource that does nothing.
+
+required_providers {
+ null = {
+  version = "~> 3.0.0"
+ }
+}
+
 resource "null_resource" "example" {
  triggers = {
   value = "A example resource that does nothing!"
