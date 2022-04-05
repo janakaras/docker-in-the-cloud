81b9542a9d4bc346e1892130432e7e8d6a73f976
janakaras <94690242+janakaras@users.noreply.github.com>
2022-04-05 10:21:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-
-     # The configuration for the `remote` backend.
-     terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "example-organization"
-
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "example-workspace"
-         }
-       }
-     }
-
      # An example resource that does nothing.
      resource "null_resource" "example" {
        triggers = {
