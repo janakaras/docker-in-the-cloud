606a5496ea14db871998b39cd58ec67a47cccb1a
janakaras <94690242+janakaras@users.noreply.github.com>
2022-04-08 16:52:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,19 @@
 terraform {
-       backend "remote" {
-              # The name of your Terraform Cloud organization.
-              organization = "test-organization-cloud-computing"
-              
-              # The name of the Terraform Cloud workspace to store Terraform state files in.
-              workspaces {
-                     name = "docker-in-the-cloud"
-              }
-       }
-       
-       required_providers {
-           aws = {
-             source  = "hashicorp/aws"
-             version = "~> 3.27"
-           }
-       }
-       
-       required_version = ">= 0.14.9"
-}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 
-# An example resource that does nothing.
-resource "null_resource" "example" {
-       triggers = {
-              value = "A example resource that does nothing!"
-       }
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
   access_key = "ASIAWEEGKEYJZJAPPQ5N"
   secret_key = "TMpgjh3gUGmWHWT8dNt7l6WPFepoePTa6j6RMUp3"
-  token = "FwoGZXIvYXdzEFAaDI0h5bIIk0z1AthBhyLNAZPEWflTLlQAP9YJtSYupb87TF75pxNcWNtMXDpbB40jl41tAH1831mB7bvvPpV2Z0ojMAYKWoD+XygkR/Fj/ALTX9vuqLPpZcrkYNLkmjns2AicEfFqsOuGXyTtDNk34SpBtpQ1UZawvrhbcfRW65zCv9obqo4krVOfOlVkYaN1lr5BY4xjTc86PpCYS7V1WaWlGuJrfe2El5We8ikw330p+fQL4L9SoTFJfGF47VuVODM9doh6f5NKXyD5TyJxEc80GPYOoX7FG1TikDMoyY/BkgYyLW3DfpB6VsfzdHPSGjW1eI2+OFw1U6KT4jOGk8sqTE4BuoRoZ91mZWBOcYD8Dw=="
-  region = "us-east-1"
+  token      = "FwoGZXIvYXdzEFAaDI0h5bIIk0z1AthBhyLNAZPEWflTLlQAP9YJtSYupb87TF75pxNcWNtMXDpbB40jl41tAH1831mB7bvvPpV2Z0ojMAYKWoD+XygkR/Fj/ALTX9vuqLPpZcrkYNLkmjns2AicEfFqsOuGXyTtDNk34SpBtpQ1UZawvrhbcfRW65zCv9obqo4krVOfOlVkYaN1lr5BY4xjTc86PpCYS7V1WaWlGuJrfe2El5We8ikw330p+fQL4L9SoTFJfGF47VuVODM9doh6f5NKXyD5TyJxEc80GPYOoX7FG1TikDMoyY/BkgYyLW3DfpB6VsfzdHPSGjW1eI2+OFw1U6KT4jOGk8sqTE4BuoRoZ91mZWBOcYD8Dw=="
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
@@ -41,7 +24,4 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
-
-
-
 
