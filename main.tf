16217db77a1905c07bb4c297513fe004fce8aa41
Greta Piliponytė <g.piliponyte97@gmail.com>
2022-04-19 18:17:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,17 +93,17 @@ resource "aws_instance" "app_server" {
   instance_type          = "t2.micro"
 #  key_name               = "ec2-deployer-key-pair"
 #   key_name = "github-actions"
-  vpc_security_group_ids = [aws_security_group.main.id]
- user_data = << EOF
-             #! /bin/bash
-             sudo yum update
-             sudo yum install docker
-             sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-             sudo chmod +x /usr/local/bin/docker-compose
-             sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose 
-             sudo service docker start
-             echo hello
-	EOF
+ vpc_security_group_ids = [aws_security_group.main.id]
+ user_data = <<EOF
+     #! /bin/bash
+     sudo yum update
+     sudo yum install docker
+     sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+     sudo chmod +x /usr/local/bin/docker-compose
+     sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose 
+     sudo service docker start
+     echo hello
+     EOF
 
 #   provisioner "remote-exec" {
 #     inline = [
