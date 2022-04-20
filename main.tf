bbe8fde83d6e298a7b4f50c971dd53a5b0913d83
janakaras <94690242+janakaras@users.noreply.github.com>
2022-04-20 21:14:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ terraform {
 
 provider "aws" {
   profile    = "default"
-  access_key = "AKIAYVQPSZZO4XMWYFE4"
-  secret_key = var.secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = "us-east-1"
 }
 
