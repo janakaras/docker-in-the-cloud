e4c1874da155e5e2b152197b6f502b8ecc861b92
Greta Piliponytė <g.piliponyte97@gmail.com>
2022-04-26 18:57:49 +0200

    Update app.py

--- gateway/app.py
+++ gateway/app.py
@@ -20,7 +20,7 @@ def find_service(name):
 # gateway hello world
 @app.route("/")
 def hello():
-    return "Hello World from the SSH!"
+    return "Hello World from the SSH2!"
 
 # Apartments
 @app.route("/apartments/apartments")
