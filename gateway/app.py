15924841083f20d6c2063db0118e55f4b2be04a9
Greta Piliponytė <g.piliponyte97@gmail.com>
2022-05-03 13:40:13 +0200

    Update app.py

--- gateway/app.py
+++ gateway/app.py
@@ -20,7 +20,7 @@ def find_service(name):
 # gateway hello world
 @app.route("/")
 def hello():
-    return "Hello World from the best team of the world! xoxo"
+    return "Hello Everybody in the Cloud Computing Lab"
 
 # Apartments
 @app.route("/apartments/apartments")
